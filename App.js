d8e4e15b352e1e7f6bd27c14f3837dc5184a7425
JunreyG <143053124+RoseMel1@users.noreply.github.com>
2023-12-09 17:24:54 +0800

    initial change

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import AddData from './screen/AddData';
+import ViewData from './screen/ViewData';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+     <Stack.Navigator>
+      <Stack.Screen name="AddData" component={ViewData}  options={{ headerShown: false }}  />
+      <Stack.Screen name="ViewData" component={ViewData}  options={{ headerShown: false }}  />
+
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  flex: 1,
+  alignContent: 'center',
+   justifyContent: 'center',
+    backgroundColor: 'white'
+    },
+  input:{
+      width: '100%'
+  }
 });
